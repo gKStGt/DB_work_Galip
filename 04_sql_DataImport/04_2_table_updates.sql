908010dd82a5a5ec25d76e0e44b6b6a72aae76df
gKStGt <galipk@arcor.de>
2020-10-29 12:03:44 +0000

    02_update

--- 04_sql_DataImport/04_2_table_updates.sql
+++ 04_sql_DataImport/04_2_table_updates.sql
@@ -4,8 +4,30 @@ system clear:
 
 SELECT * FROM cats;
 
-/* Eingrenzen durch WHERE */
+/* Eingrenzen/Filtern durch WHERE */
+
+/* Update */
 #UPDATE cats SET name="Alonzo" WHERE id=2;
-UPDATE cats SET name="Alonzo" WHERE fur_color="grey";
+#UPDATE cats SET name="Alonzo" WHERE fur_color="grey";
+UPDATE cats SET name="Alonzo",fur_color="red" WHERE name="Alonza";
+#SELECT * FROM cats;
+
+/* Select + Modifikation mit AS */
+
+#SELECT * FROM cats; # gesammte Tabelle
+#SELECT name FROM cats; # Feld name
+#SELECT name,age FROM cats; # Feld name + age 
+#SELECT age,name FROM cats; # Feld age + name
+#SELECT name AS "Name", age AS "Alter der Katze" FROM cats; # Feld name + age 
+
+SELECT name,age FROM cats WHERE id=1;
+SELECT name "Namen",fur_color "Fellfarbe" FROM cats WHERE fur_color="red";
+SELECT name "Name",age "Alter" FROM cats WHERE age >=40;
+
+/* DELETE */
+
+DELETE FROM cats WHERE id=1;
+DELETE FROM cats WHERE age >=40;
 
 SELECT * FROM cats;
+
